2d2bb1a1f60603ebc0921847245e85d333d658c9
jayzizzle <dj.jonzamora@gmail.com>
2021-09-24 19:13:17 -0700

    add session api util

--- frontend/ripple.jsx
+++ frontend/ripple.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// TESTING ONLY
+import * as APIUtil from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
+
+  //TESTING ONLY
+  window.signup = APIUtil.signup;
+  window.login = APIUtil.login;
+  window.logout = APIUtil.logout;
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Ripple</h1>, root);
 });
