42a9ef5503a74b4bf2c9239026eb1188123b27f1
jayzizzle <dj.jonzamora@gmail.com>
2021-09-25 08:04:43 -0700

    add session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,43 @@
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVED_CURRENT_USER = 'RECEIVED_CURRENT_USER';
+export const LOGGED_OUT_CURRENT_USER = 'LOGGED_OUT_CURRENT_USER';
+export const RECEIVED_SESSION_ERRORS = 'RECEIVED_SESSION_ERRORS';
+
+export const receivedCurrentUser = currentUser => ({
+  type: RECEIVED_CURRENT_USER,
+  currentUser
+});
+
+export const loggedOutCurrentUser = () => ({
+  type: LOGGED_OUT_CURRENT_USER
+});
+
+export const receivedSessionErrors = errors => ({
+  type: RECEIVED_SESSION_ERRORS,
+  errors
+});
+
+export const signup = user => dispatch => (
+  APIUtil.signup(user).then(currentUser => (
+    dispatch(receivedCurrentUser(currentUser))
+  ), error => (
+    dispatch(receivedSessionErrors(error.responseJSON))
+  ))
+);
+
+export const login = user => dispatch => (
+  APIUtil.login(user).then(currentUser => (
+    dispatch(receivedCurrentUser(currentUser))
+  ), error => (
+    dispatch(receivedSessionErrors(error.responseJSON))
+  ))
+);
+
+export const logout = () => dispatch => (
+  APIUtil.logout().then(() => (
+    dispatch(loggedOutCurrentUser())
+  ), error => (
+    dispatch(receivedSessionErrors(error.responseJSON))
+  ))
+);
