8ae2197b041ad98f95cd86f49bdf1b75f47f3be4
jayzizzle <dj.jonzamora@gmail.com>
2021-10-06 22:11:17 -0700

    add likes controllers

---
+++ app/controllers/api/album_likes_controller.rb
@@ -0,0 +1,42 @@
+class Api::AlbumLikesController < ApplicationController
+
+  before_action :underscore_params!
+
+  def index
+    @album_likes = current_user.album_likes
+    render '/api/album_likes/index'
+  end
+
+  def show
+    @album_like = AlbumLike.find_by(id: params[:id])
+    if @album_like
+      render '/api/album_likes/show'
+    else
+      render json: ['Like does not exist'], status: 404
+    end
+  end
+
+  def create
+    @album_like = AlbumLike.new(album_likes_params)
+    if @album_like.save
+      render '/api/album_likes/show'
+    else
+      render json: @album_like.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @album_like = AlbumLike.find_by(id: params[:id])
+    if @album_like.destroy
+      render '/api/album_likes/show'
+    else
+      render json: ['Like does not exist'], status: 404
+    end
+  end
+
+  private
+  def album_likes_params
+    params.require(:album_likes).permit(:user_id, :album_id)
+  end
+
+end
---
+++ app/controllers/api/track_likes_controller.rb
@@ -0,0 +1,42 @@
+class Api::TrackLikesController < ApplicationController
+
+  before_action :underscore_params!
+
+  def index
+    @track_likes = current_user.track_likes
+    render '/api/track_likes/index'
+  end
+
+  def show
+    @track_like = TrackLike.find_by(id: params[:id])
+    if @track_like
+      render '/api/track_likes/show'
+    else
+      render json: ['Like does not exist'], status: 404
+    end
+  end
+
+  def create
+    @track_like = TrackLike.new(track_likes_params)
+    if @track_like.save
+      render '/api/track_likes/show'
+    else
+      render json: @track_like.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @track_like = TrackLike.find_by(id: params[:id])
+    if @track_like.destroy
+      render '/api/track_likes/show'
+    else
+      render json: ['Like does not exist'], status: 404
+    end
+  end
+
+  private
+  def track_likes_params
+    params.require(:track_likes).permit(:user_id, :track_id)
+  end
+
+end
